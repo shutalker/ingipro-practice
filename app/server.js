ed34fefff4bb044f21ed214c89f47fba081a52ac
Irina Marchuk <mar4ukira@gmail.com>
2017-07-25 23:49:39 +0300

    add linters

--- app/server.js
+++ app/server.js
@@ -5,5 +5,6 @@ const path = require('path');
 app.use(express.static(path.join(__dirname, 'views')));
 
 app.listen(3000, () => {
+    // eslint-disable-next-line
     console.log('Server started on localhost:3000');
 });
