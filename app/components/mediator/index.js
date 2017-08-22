3aee185d18d818f0c4e81b55385671797518b70f
Sergey <mr.smolya@yandex.ru>
2017-08-22 21:07:18 +0300

    Modify mediator (#13)
    * mediator

--- app/components/mediator/index.js
+++ app/components/mediator/index.js
@@ -1,22 +1,48 @@
+'use strict';
+
 class Mediator {
     constructor() {
-        // @fixme remove `console.log`
-        // eslint-disable-next-line
-        console.log('"Mediator" created');
-    }
-    
-    on(type, listener) {
-        // @fixme remove `console.log`
-        // eslint-disable-next-line
-        console.log('mediator.on', type, listener)
+        this._listeners = {};
     }
 
-    emit(type, ...args) {
-        // @fixme remove `console.log`
-        // eslint-disable-next-line
-        console.log('mediator.emit', type, args)
+    on(type, listener) {
+        if (!this._listeners[type]) {
+            this._listeners[type] = [];
+        }
+
+        if ( this._listeners[type].includes(listener) ) {
+            return;
+        }
+
+        this._listeners[type].push(listener);
+    }
+
+    off(type, listener) {
+        if (this._listeners[type]) {
+            for (let i = 0; i < this._listeners[type].length; i++) {
+                if(this._listeners[type][i] === listener) {
+                    this._listeners[type].splice(i, 1);
+                    return;
+                }
+            }
+        }
+    }
+
+    emit(type, data) {
+        let sliceChar = type.slice( 0, type.indexOf(':') ) + ': *';
+
+        if ( this._listeners[sliceChar] ) {
+            this._listeners[sliceChar].forEach( listener => listener.call(null, data, type) );
+        }
+
+        if (this._listeners['*']) {
+            this._listeners['*'].forEach( listener => listener.call(null, data, type) );
+        }
+
+        if (this._listeners[type]) {
+            this._listeners[type].forEach( listener => listener.call(null, data, type) );
+        } 
     }
 }
 
-// kind of Singleton pattern
 export default new Mediator();
