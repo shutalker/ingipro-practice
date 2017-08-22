661fa0bab330c872eb8548e3140717f314a41305
dovyden <dovyden@yandex.ru>
2017-08-22 20:54:05 +0300

    fixed building of static (#17)
    * add voice component and editor config
    * fixed building of static

---
+++ app/components/voice/index.js
@@ -0,0 +1,10 @@
+class Voice {
+    constructor() {
+        // @fixme remove `console.log`
+        // eslint-disable-next-line
+        console.log('"Voice" created');
+    }
+}
+
+// kind of Singleton pattern
+export default new Voice();
